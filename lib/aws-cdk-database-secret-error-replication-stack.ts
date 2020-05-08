da3b459d0834377c914294bea159911da80f9343
Blaine Price <blaine@ario.com>
2020-05-08 18:48:24 -0400

    setup minimal example

--- lib/aws-cdk-database-secret-error-replication-stack.ts
+++ lib/aws-cdk-database-secret-error-replication-stack.ts
@@ -1,9 +1,25 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { Vpc, InstanceClass, InstanceSize, InstanceType } from "@aws-cdk/aws-ec2";
+import { DatabaseInstance, DatabaseInstanceEngine } from "@aws-cdk/aws-rds";
+import { Duration } from "@aws-cdk/core";
 
 export class AwsCdkDatabaseSecretErrorReplicationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, "TestVPC");
+
+    const database = new DatabaseInstance(this, "testDatbase", {
+      instanceIdentifier: "testDatabase",
+      engine: DatabaseInstanceEngine.POSTGRES,
+      engineVersion: "10.6",
+      instanceClass: InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO),
+      masterUsername: "gladmin",
+      vpc,
+      backupRetention: Duration.days(30),
+      multiAz: true
+    });
+
+    database.addRotationSingleUser();
   }
 }
